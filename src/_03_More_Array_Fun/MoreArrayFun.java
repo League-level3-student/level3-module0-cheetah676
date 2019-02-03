afdd51afd735abb2e9967965a2c6e41c7739e9a0
cheetah676 <league@WTS20.attlocal.net>
2019-02-03 11:25:44 -0800

    firework display, simple array methods, more array fun ip

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -2,20 +2,28 @@ package _03_More_Array_Fun;
 
 import java.util.Random;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class MoreArrayFun {
+	static String [] yee={"yee", "hi", "yo"};
 	//1. Create a main method to test the other methods you write.
-	
-	
-	
+	public static void main(String[]args) {
+	printStringArray(yee);
+	printStringArrayInReverse(yee);
+	}
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
-	
-	
+	static void printStringArray(String[] array){
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
-	
-	
+	static void printStringArrayInReverse(String[] array) {
+		for (int i = array.length-1; i >= 0; i--) {
+			System.out.println(array[i]);
+		}
+	}
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
 	
 	
