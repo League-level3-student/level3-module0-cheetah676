4895957284ed4a4282074f0aa6307d21aab6540a
cheetah676 <league@WTS20.attlocal.net>
2019-02-10 11:27:07 -0800

    finished level3 module 1

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -5,11 +5,13 @@ import java.util.Random;
 import javax.swing.text.StyledEditorKit.ForegroundAction;
 
 public class MoreArrayFun {
-	static String [] yee={"yee", "hi", "yo"};
+	static String [] yee={"1", "2", "3", "4"};
 	//1. Create a main method to test the other methods you write.
 	public static void main(String[]args) {
 	printStringArray(yee);
 	printStringArrayInReverse(yee);
+	printEveryOtherString(yee);
+	printStringsRandomly(yee);
 	}
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	static void printStringArray(String[] array){
@@ -25,10 +27,28 @@ public class MoreArrayFun {
 		}
 	}
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
+	static void printEveryOtherString(String[] array) {
+		for(int i=0; i<array.length; i+=2) {
+			System.out.println(array[i]);
+		}
+	}
 	
 	//5. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in a completely random order. Almost every run of the program should result in a different order.
-	
+	static void printStringsRandomly(String[]array) {
+		for(int i=0; i<array.length; i++) {
+			int size=4;
+			boolean[]seen=new boolean[4];
+			int count=0;
+			while(count<size) {
+				Random random=new Random();
+				int randomInt=random.nextInt(array.length);
+				if(seen[randomInt]==false) {
+					System.out.println(randomInt);
+					seen[randomInt]=true;
+				}
+			}
+		}
+	}
 	
 }
